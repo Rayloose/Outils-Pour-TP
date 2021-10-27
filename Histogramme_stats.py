be8790400ca3089c9ac05b03e00bd9ae16ae1f0f
Rayloose <jeremiehoffner@gmail.com>
2021-10-27 12:57:29 +0200

    Ajout des noms des variables lors du print

--- Histogramme_stats.py
+++ Histogramme_stats.py
@@ -1,26 +1,24 @@
-from os import name
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 A = ["Foyer Objet",np.array([1,1.15]) , "m"] #exemple : nom de la variable, valeurs, unité(s)
-B = ["Foyer image",np.array([12,24]) , "mL"]
-C = ["Foyer",np.array([0.001,0.002]) , "mol"]
+B = ["Volume",np.array([12,24]) , "mL"]
+C = ["Concentration",np.array([0.001,0.002]) , "mol"]
 
 a = 1
 
 for i in A,B,C :
 
+    nom = i[0]
     valeurs = i[1] #Changement de la variable à étudier
     unite = i[2]
 
     #affichage des stats
-    print("----","SERIE",a,"----")
-    print("Nombre de mesures : ",len(valeurs))
-    print("Etendue : ",max(i[1])-min(i[1]))
+    print("----","SERIE",a,":",nom,"----")
+    print("Nombre de mesures :",len(valeurs))
+    print("Etendue :",max(i[1])-min(i[1]))
     print("Médiane :", np.median(valeurs))
-    print("Valeur moyenne : ", np.mean(valeurs) , unite)
+    print("Valeur moyenne :", np.mean(valeurs) , unite)
     print("Ecart-type:", np.std(valeurs,ddof=1) ,unite)
     print("Incertitude-type:", np.std(valeurs,ddof=1) /(len(valeurs))**0.5 ,unite)
     print("Incertitude élargie, 95% :",2* np.std(valeurs,ddof=1) /(len(valeurs))**0.5 ,unite)
@@ -30,7 +28,7 @@ for i in A,B,C :
     #Affichage du graphique
 
 
-for i in A,B,C :
+for i in [] :
 
     plt.hist(i[1])
     plt.title("Histogramme des valeurs")
