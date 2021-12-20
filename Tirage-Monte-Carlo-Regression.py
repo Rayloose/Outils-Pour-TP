d116c1050bb80f10197cc0c595d3279e389f9ba0
Rayloose <jeremiehoffner@gmail.com>
2021-12-20 11:28:31 +0100

    Modification des entrées, ajout des barres d'erreurs, une seule variable pour les incertitudes

--- Tirage-Monte-Carlo-Regression.py
+++ Tirage-Monte-Carlo-Regression.py
@@ -3,31 +3,26 @@ import matplotlib.pyplot as plt
 import numpy.random as rd
 
 N = 10**3
-# exemple : nom de la variable, valeurs, unité(s)
-A = ["Volume", np.array([12, 24, 32, 44]), "L"]
-B = ["Incertitude Volume", np.array([0.1]*len(A)), "L"]
-print(B)
-C = ["Concentration", np.array([0.1, 0.2, 0.4, 0.7]), "mol"]
-D = ["Incertitude concentration", np.array([0.1]*len(C)), "mol"]
+decimale = 5
+X = ["Volume", np.array([12, 24, 32, 44]), "L",2]
+Y = ["Quantité de CO2 dégagé", np.array([0.1, 0.2, 0.4, 0.7]), "mol",0.1]
 
-X = A[1]
-u_X = B[1]
-Y = C[1]
-u_Y = D[1]
+# exemple : nom de la variable, valeurs, unité(s), incertitudes
 
 a = np.zeros(N)
 b = np.zeros(N)
 
 for i in range(N):
-    Xi = rd.normal(X, u_X)
-    Yi = rd.normal(Y, u_Y)
+    Xi = rd.normal(X[1], X[3])
+    Yi = rd.normal(Y[1], Y[3])
     a[i], b[i] = np.polyfit(Xi, Yi, 1)
 
-print("Valeur moyenne pour A:", a.mean())
-print("Incertitude-type sur A :", a.std())
-print("Valeur moyenne pour B:", b.mean())
-print("Incertitude-type sur B :", b.std())
+print('regression linéaire de type Y = AX + B')
+print("A :", round(a.mean(),decimale),"+-",round(a.std(),decimale))
+print(" B :", round(b.mean(),decimale),"+-",round(b.std(),decimale))
 
-plt.plot(Xi, Yi, label="Valeurs avec tirages")
+plt.errorbar(X[1],Y[1], xerr= X[3], yerr = Y[3])
+plt.xlabel("à compléter")
+plt.ylabel("à compléter")
 plt.plot(Xi, Xi*a.mean()+b.mean(), label="Regression")
 plt.show()
